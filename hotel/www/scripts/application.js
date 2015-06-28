43c399e166f15ab75d8c9623cfa132d8f9df82d8
Dhiraj Gupts <dhiraj.gupta23@gmail.com>
2015-06-29 01:28:00 +0530

    adds tabs and json data

--- hotel/www/scripts/application.js
+++ hotel/www/scripts/application.js
@@ -13,34 +13,84 @@ angular.module('SteroidsApplication', [
         })
     }
 
+    this.getAllProducts = function(){
+        return $http.get('/scripts/data/getProducts.json').then(function(data){
+            return data.data;
+        })
+    }
+
 })
 
 .controller('IndexController', function($scope, supersonic, Product) {
 
-    //supersonic.ui.screen.setAllowedRotations(["landscapeLeft", "landscapeRight"]);
+    $scope.selectedTab = 'sandwich';
 
     $scope.navbarTitle = "dd";
 
+    $scope.allProducts = {};
+
     function init(){
         console.log('init')
-        Product.getProductsByCategory('sandwiches').then(function(data){
-            alert(data)
-            console.log(data);
+        Product.getAllProducts().then(function(data){
+            var rows = Math.ceil(data.length/3);
+            //for (var i = 0; i < rows; i++) {
+              //  var cols = data.slice(i*3,(i*3)+3);
+
+            
+            var indexes = {};
+
+            for (var i = 0; i < data.length; i++) {
+                //data[i]
+
+
+
+                if(!$scope.allProducts.hasOwnProperty(data[i].category)){
+                    $scope.allProducts[data[i].category] = [];
+                    indexes[data[i].category] = 0;
+                    //$scope.allProducts[data[i].category][parseInt(indexes[data[i].category])] = [data[i]];
+                    $scope.allProducts[data[i].category][parseInt(indexes[data[i].category])] = [];
+                    $scope.allProducts[data[i].category][parseInt(indexes[data[i].category])].push(data[i]);
+                }else{
+
+                    
+                    if($scope.allProducts[data[i].category][parseInt(indexes[data[i].category])].length==3){
+                        $scope.allProducts[data[i].category].push([data[i]]);
+                        indexes[data[i].category] += 1;
+                    }else{
+                        $scope.allProducts[data[i].category][parseInt(indexes[data[i].category])].push(data[i]);
+                    }
+                } 
+            };
+
         })
+    }
+
+    $scope.addProduct = function(e){
+        var val = angular.element(e.currentTarget).find('button').text();
+        if(isNaN(val)){
+            val = 1;
+        }else{
+            val = parseInt(val)
+            val++;
+        }
+        angular.element(e.currentTarget).find('button').text(val);
+    }
+
+    $scope.changeTab = function(e){
+        $scope.selectedTab = angular.element(e.currentTarget).attr('tab')
     }
 
 
     $scope.openLeftMenu = function(){
 
-        Product.getProductsByCategory('sandwiches').then(function(data){
-            alert(data)
-            console.log(data);
-        })
+        
 
     	supersonic.ui.drawers.open("left").then( function() {
 			supersonic.logger.debug("Drawer was shown");
 		});
     }
+
+    init()
 
    
 
